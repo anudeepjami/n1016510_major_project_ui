ae38222e5d0d38417d59fc1d449db34868f26d2b
N1016510 <anudeep.jami@gmail.com>
2022-08-15 14:03:06 +0100

    bug fixes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Navbar, Container, Nav } from 'react-bootstrap';
+import HomePage from './pages/HomePage';
+import TestPage from './pages/TestPage';
 
 function App() {
   return (
     <div>
-      <h1>Anudeep Jami Mahor Project</h1>
+      <Navbar bg="dark">
+        <Container>
+          <Navbar.Brand href="/"><img width="200" height="100" src={require('./images/companylogo.png')} /></Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link style={{color: "white"}} href="/">Home</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+      <Router>
+        <Routes>
+          <Route path='/' element={<HomePage />} />
+        </Routes>
+      </Router>
     </div>
   )
 }
