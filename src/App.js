92efcfe336b04917ccb735ceaff35e95791f8dde
N1016510 <anudeep.jami@gmail.com>
2022-08-18 20:28:57 +0100

    ui modification

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react';
 import { useCookies } from 'react-cookie';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { Navbar, Container, Nav, Button } from 'react-bootstrap';
+import { Navbar, Container, Nav, Button, Card, ListGroup } from 'react-bootstrap';
 import HomePage from './pages/HomePage';
 import CreateFundingEventPage from './pages/CreateFundingEventPage';
 import FundingPage from './pages/FundingPage';
@@ -35,15 +35,27 @@ function App() {
   return (
     <div>
       <Navbar bg="dark">
-        <Container>
+        <Container className="d-flex justify-content-between">
           <Navbar.Brand href="/"><img width="400" height="100" src={require('./images/companylogo.png')} /></Navbar.Brand>
-          <Nav className="me-auto">
-            <Nav.Link style={{ color: "white" }} href="/">Home</Nav.Link>
-            <Nav.Link style={{ color: "white" }} href="/user">User</Nav.Link>
-          </Nav>
-
-          <Nav className="justify-content-end">
+          <Nav>
             <Nav.Link href="/create"><Button variant="primary">Create a Crowdfunding Campaign</Button></Nav.Link>
+            <Nav.Link style={{ color: "white" }} href="/user"><Button variant="primary">Profile</Button></Nav.Link>
+          </Nav>
+          <Nav>
+            <Navbar.Text>
+              <Card>
+                <Card.Header>
+                  {cookies.MetamaskNetwork != undefined ? "Ethereum Network: " : ""}<b>
+                  {cookies.MetamaskNetwork == "0x4" ? "Rinkeby" :
+                    cookies.MetamaskNetwork == "0x1691" ? "Ganache Local" : "Only Rinkeby and local Ganache Supported"}</b>
+                </Card.Header>
+                <ListGroup variant="flush">
+                  <ListGroup.Item><b>
+                    {cookies.MetamaskLoggedInAddress != undefined ? cookies.MetamaskLoggedInAddress : "Read only mode enabled as Metamask is not logged in"}
+                  </b></ListGroup.Item>
+                </ListGroup>
+              </Card>
+            </Navbar.Text>
           </Nav>
         </Container>
       </Navbar>
